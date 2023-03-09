e75afa55949eb4546b8b82861bb980be192c6bf0
Willem Meijer <w.meijer.5@student.rug.nl>
2023-03-09 12:46:41 +0000

    improved error handling; reduced cpu usage.

--- python_proj/experiment/issue_grabber.py
+++ python_proj/experiment/issue_grabber.py
@@ -1,6 +1,9 @@
 import json
 from os import getenv
 import dotenv
+import traceback
+from time import sleep
+from requests import HTTPError
 
 from perceval.backends.core.github import GitHub, CATEGORY_PULL_REQUEST, CATEGORY_ISSUE
 from watchdog.events import FileSystemEventHandler, FileClosedEvent
@@ -9,6 +12,9 @@ from watchdog.observers import Observer
 dotenv.load_dotenv()
 github_token = getenv("GITHUB_TOKEN")
 github_token_2 = getenv("GITHUB_TOKEN_2")
+
+exception_output_file = open(
+    "./data/col_exceptions.out", "a+", encoding="utf-8")
 
 
 class NewNPMProjHandler(FileSystemEventHandler):
@@ -20,7 +26,8 @@ class NewNPMProjHandler(FileSystemEventHandler):
 
         with open(src_path, "r", encoding="utf-8") as input_file:
             entry = json.loads(input_file.read())
-            print('Getting PRs and issues for {name}'.format(name=entry['id']))
+            print('\nGetting PRs and issues for {name}'.format(
+                name=entry['id']))
             repo = self.make_repo(entry)
             self.grab_pull_requests(entry, repo)
             self.grab_issues(entry, repo)
@@ -45,13 +52,23 @@ class NewNPMProjHandler(FileSystemEventHandler):
 
             with open(output_path, "w+", encoding="utf-8") as output_file:
 
+                pr_count = 0
                 for count, pr in enumerate(repo.fetch(category=CATEGORY_PULL_REQUEST)):
                     output_file.write(json.dumps(pr) + "\n")
+                    pr_count = count
 
                 print("Collected {count} PRs for {name}.".format(
-                    name=entry['id'], count=count))
-        except:
+                    name=entry['id'], count=pr_count))
+        except HTTPError as herr:
+            if herr.response.status_code == 404:
+                print("Repo for {name} doesn't exist.".format(
+                    name=entry['id']))
+        except Exception as e:
             print("Could not get PRs for {name}.".format(name=entry['id']))
+            exception_output_file.write(
+                '\n\nPR failure for {name}'.format(name=entry['id']))
+            traceback.print_exc(file=exception_output_file)
+            exception_output_file.flush()
 
     def grab_issues(self, entry: str, repo: GitHub):
         try:
@@ -59,13 +76,19 @@ class NewNPMProjHandler(FileSystemEventHandler):
                 type="issue", name=entry["id"])
             with open(output_path, "w+", encoding="utf-8") as output_file:
 
+                iss_count = 0
                 for count, issue in enumerate(repo.fetch(category=CATEGORY_ISSUE)):
                     output_file.write(json.dumps(issue) + "\n")
+                    iss_count = count
 
             print("Collected {count} issues for {name}.".format(
-                name=entry['id'], count=count))
-        except:
+                name=entry['id'], count=iss_count))
+        except Exception as e:
             print("Could not get issues for {name}.".format(name=entry['id']))
+            exception_output_file.write(
+                '\n\nIssue failure for {name}'.format(name=entry['id']))
+            traceback.print_exc(file=exception_output_file)
+            exception_output_file.flush()
 
 
 print("Starting with PR/issue collecting.")
@@ -73,13 +96,17 @@ print("Starting with PR/issue collecting.")
 watch_path = "./data/npm/"
 output_path = "./data/{type}/{name}.json"
 
+
 proj_handler = NewNPMProjHandler(output_path)
 observer = Observer()
 observer.schedule(proj_handler, path=watch_path, recursive=False)
 observer.start()
 
-while True:
+is_running = True
+while is_running:
     try:
-        pass
+        sleep(1)
     except KeyboardInterrupt:
+        print("Stopping...")
+        is_running = False
         observer.stop()
