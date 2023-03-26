882a954cba4dc294ea38286d2e4196f98af37448
Willem Meijer <w.meijer.5@student.rug.nl>
2023-03-26 16:46:58 +0000

    upped number of tokens.

--- python_proj/experiment/replication_study/retrieve_pull_requests.py
+++ python_proj/experiment/replication_study/retrieve_pull_requests.py
@@ -44,7 +44,7 @@ end_date = datetime(year=2020, month=1, day=12)
 
 dotenv.load_dotenv()
 
-all_gh_tokens = [getenv(f"GITHUB_TOKEN_{i}") for i in range(1, 11)]
+all_gh_tokens = [getenv(f"GITHUB_TOKEN_{i}") for i in range(1, 21)]
 gitlab_token_1 = getenv("GITLAB_TOKEN_1")
 
 all_gl_tokens = [gitlab_token_1]
