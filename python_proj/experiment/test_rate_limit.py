d09d7bd82bc08375b35c167fbdf28fc07c929b39
Willem Meijer <w.meijer.5@student.rug.nl>
2023-03-27 10:12:10 +0000

    minor refactor

--- python_proj/experiment/test_rate_limit.py
+++ python_proj/experiment/test_rate_limit.py
@@ -4,21 +4,28 @@ from os import getenv
 import requests
 import json
 
-dotenv.load_dotenv()
 
-all_gh_tokens = [getenv(f"GITHUB_TOKEN_{i}") for i in range(1, 21)]
+def test_progress():
+    dotenv.load_dotenv()
 
-for index, token in enumerate(all_gh_tokens, start=1):
-    url = "https://api.github.com/rate_limit"
-    headers = {
-        "Accept": "application/vnd.github+json",
-        "Authorization": f'Bearer {token}',
-        "X-GitHub-Api-Version": "2022-11-28"
-    }
+    all_gh_tokens = [getenv(f"GITHUB_TOKEN_{i}") for i in range(1, 21)]
 
-    response = requests.get(url, headers=headers)
-    j_response = json.loads(response.text)
+    for index, token in enumerate(all_gh_tokens, start=1):
+        url = "https://api.github.com/rate_limit"
+        headers = {
+            "Accept": "application/vnd.github+json",
+            "Authorization": f'Bearer {token}',
+            "X-GitHub-Api-Version": "2022-11-28"
+        }
 
-    rate = j_response["rate"]
+        response = requests.get(url, headers=headers)
+        j_response = json.loads(response.text)
 
-    print(f'Token {index}: used {rate["used"]} of {rate["limit"]} requests.')
+        rate = j_response["rate"]
+
+        print(
+            f'Token {index}: used {rate["used"]} of {rate["limit"]} requests.')
+
+
+if __name__ == "__main__":
+    test_progress()
