8888a6b02c94594c903a2ba1e17f405ad3273cb6
Willem Meijer <w.meijer.5@student.rug.nl>
2023-04-02 09:53:03 +0000

    fix minor error

--- python_proj/experiment/replication_study/filter_projects.py
+++ python_proj/experiment/replication_study/filter_projects.py
@@ -78,7 +78,7 @@ def has_sufficient_closed_prs(owner, repo_name, threshold: int, host_type: str) 
                 except:
                     tformat = "%Y-%m-%dT%H:%M:%SZ"
                     updated_at = datetime.strptime(pr["updated_at"], tformat)
-                    created_at
+                    created_at = datetime.strptime(pr["created_at"], tformat)
                 if created_at > max_pr_date or updated_at > max_pr_date:
                     continue
                 count += 1
