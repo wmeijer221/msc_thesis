be2f10798a88ecf01c3beaf0787e970c326c9737
Willem Meijer <w.meijer.5@student.rug.nl>
2023-04-21 07:58:18 +0000

    naming

--- python_proj/experiment/data_sorter.py
+++ python_proj/experiment/data_sorter.py
@@ -22,7 +22,7 @@ if not path.exists(temp_dir):
     makedirs(temp_dir)
 
 
-def get_nested(dictionary: dict, recursive_key: list[str]):
+def _get_nested(dictionary: dict, recursive_key: list[str]):
     current = dictionary
     for key in recursive_key:
         if not key in current:
@@ -31,7 +31,7 @@ def get_nested(dictionary: dict, recursive_key: list[str]):
     return current
 
 
-def iterate_and_split(filter_path: str, datetime_key: list[str]) -> set[str]:
+def _iterate_and_split(filter_path: str, datetime_key: list[str]) -> set[str]:
     ymds = set()
     with open(filter_path, "r") as filter_file:
         for file in filter_file:
@@ -49,7 +49,7 @@ def iterate_and_split(filter_path: str, datetime_key: list[str]) -> set[str]:
                 j_data = json.loads(entries_file.read())
                 for entry in j_data:
                     try:
-                        closed_at = get_nested(entry, datetime_key)
+                        closed_at = _get_nested(entry, datetime_key)
                         if closed_at is None:
                             print(
                                 f'Skipping entry without feature "{feature_name}" {repo_split}.')
@@ -76,11 +76,11 @@ def iterate_and_split(filter_path: str, datetime_key: list[str]) -> set[str]:
     return ymds
 
 
-def parallel_sort(ymds: set[str]):
-    def sort_key(entry: dict):
-        return get_nested(entry, datetime_key)
+def _parallel_sort(ymds: set[str]):
+    def _sort_key(entry: dict):
+        return _get_nested(entry, datetime_key)
 
-    def sort_entries(task: str, task_id: int, total_tasks: int):
+    def _sort_entries(task: str, task_id: int, total_tasks: int):
         ymd = task
         print(f'Starting with task ({task_id}/{total_tasks}) "{ymd}"')
         bucket_path = temp_storage_path.format(bucket=ymd)
@@ -88,16 +88,16 @@ def parallel_sort(ymds: set[str]):
         with open(bucket_path, "r") as bucket_file:
             entries = [json.loads(line.strip()) for line in bucket_file]
         remove(bucket_path)
-        entries.sort(key=sort_key)
+        entries.sort(key=_sort_key)
         # Writes sorted data
         with open(bucket_path, 'w+') as bucket_file:
             newlines = [f'{json.dumps(entry)}\n' for entry in entries]
             bucket_file.writelines(newlines)
 
-    parallelize_tasks(ymds, sort_entries, thread_count)
+    parallelize_tasks(ymds, _sort_entries, thread_count)
 
 
-def write_sorted_buckets(ymds: set):
+def _write_sorted_buckets(ymds: set):
     sorted_ymds = list(ymds)
     sorted_ymds.sort()
     r_output_path = output_path.format(eco=eco_name, feature=feature_name)
@@ -114,13 +114,13 @@ def sort_data(file_name: str, datetime_key: list[str], feature_name: str, eco_na
     real_base_path = base_path.format(eco=eco_name, feature=feature_name)
     
     print("Starting bucket creation.")
-    ymds = iterate_and_split(file_name, datetime_key)
+    ymds = _iterate_and_split(file_name, datetime_key)
     
     print("Starting parallel bucket sort.")
-    parallel_sort(ymds)
+    _parallel_sort(ymds)
     
     print("Merging buckets.")
-    write_sorted_buckets(ymds)
+    _write_sorted_buckets(ymds)
     print("Done!")
 
 
