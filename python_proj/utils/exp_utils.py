a82390c99b092e2b1f1c4bee5c57127b3942033b
Willem <w.meijer.5@student.rug.nl>
2023-05-12 13:09:20 +0000

    format

--- python_proj/utils/exp_utils.py
+++ python_proj/utils/exp_utils.py
@@ -19,6 +19,29 @@ PROJECTS_WITH_REPO_PATH: str | partial[str] = \
 RAW_DATA_PATH: str | partial[str] = "./data/libraries/{eco}-libraries-1.6.0-2020-01-12/{data_type}/{owner}--{repo}{ext}.json"
 FILTER_PATH: str | partial[str] = "./data/libraries/{eco}-libraries-1.6.0-2020-01-12/predictors/included_projects{filter_type}.csv"
 
+LIBRARIES_IO_DATASET_END_DATE = datetime(year=2020, month=1, day=12)
+
+PROJECTS_WITH_REPOSITORY_FIELDS_HEADERS = \
+    ['ID', 'Platform', 'Name', 'Created Timestamp', 'Updated Timestamp',
+     'Description', 'Keywords', 'Homepage URL', 'Licenses', 'Repository URL',
+     'Versions Count', 'SourceRank', 'Latest Release Publish Timestamp',
+     'Latest Release Number', 'Package Manager ID', 'Dependent Projects Count',
+     'Language', 'Status', 'Last synced Timestamp', 'Dependent Repositories Count',
+     'Repository ID', 'Repository Host Type', 'Repository Name with Owner', 'Repository Description',
+     'Repository Fork?', 'Repository Created Timestamp', 'Repository Updated Timestamp',
+     'Repository Last pushed Timestamp', 'Repository Homepage URL', 'Repository Size',
+     'Repository Stars Count', 'Repository Language', 'Repository Issues enabled?',
+     'Repository Wiki enabled?', 'Repository Pages enabled?', 'Repository Forks Count',
+     'Repository Mirror URL', 'Repository Open Issues Count', 'Repository Default branch',
+     'Repository Watchers Count', 'Repository UUID', 'Repository Fork Source Name with Owner',
+     'Repository License', 'Repository Contributors Count', 'Repository Readme filename',
+     'Repository Changelog filename', 'Repository Contributing guidelines filename',
+     'Repository License filename', 'Repository Code of Conduct filename',
+     'Repository Security Threat Model filename', 'Repository Security Audit filename',
+     'Repository Status', 'Repository Last Synced Timestamp', 'Repository SourceRank',
+     'Repository Display Name', 'Repository SCM type', 'Repository Pull requests enabled?',
+     'Repository Logo URL', 'Repository Keywords']
+
 
 def load_paths_for_eco(eco_key: str = ECO_KEY):
     global PROJECTS_WITH_REPO_PATH, RAW_DATA_PATH, FILTER_PATH
@@ -34,7 +57,7 @@ def load_paths_for_data_path(data_source_key: str = DATA_SOURCE_KEY):
     global RAW_DATA_PATH
 
     data_source = safe_get_argv(data_source_key, "pull-requests")
-    # Assumes the eco has been loaded already.
+    # Assumes ``load_paths_for_eco`` has been called.
     RAW_DATA_PATH = partial(RAW_DATA_PATH, data_type=data_source)
 
 
@@ -66,27 +89,3 @@ def get_gl_token() -> str:
     if token is None:
         raise Exception("GL token is none!")
     return token
-
-
-LIBRARIES_IO_DATASET_END_DATE = datetime(year=2020, month=1, day=12)
-
-PROJECTS_WITH_REPOSITORY_FIELDS_HEADERS = \
-    ['ID', 'Platform', 'Name', 'Created Timestamp', 'Updated Timestamp',
-     'Description', 'Keywords', 'Homepage URL', 'Licenses', 'Repository URL',
-     'Versions Count', 'SourceRank', 'Latest Release Publish Timestamp',
-     'Latest Release Number', 'Package Manager ID', 'Dependent Projects Count',
-     'Language', 'Status', 'Last synced Timestamp', 'Dependent Repositories Count',
-     'Repository ID', 'Repository Host Type', 'Repository Name with Owner', 'Repository Description',
-     'Repository Fork?', 'Repository Created Timestamp', 'Repository Updated Timestamp',
-     'Repository Last pushed Timestamp', 'Repository Homepage URL', 'Repository Size',
-     'Repository Stars Count', 'Repository Language', 'Repository Issues enabled?',
-     'Repository Wiki enabled?', 'Repository Pages enabled?', 'Repository Forks Count',
-     'Repository Mirror URL', 'Repository Open Issues Count', 'Repository Default branch',
-     'Repository Watchers Count', 'Repository UUID', 'Repository Fork Source Name with Owner',
-     'Repository License', 'Repository Contributors Count', 'Repository Readme filename',
-     'Repository Changelog filename', 'Repository Contributing guidelines filename',
-     'Repository License filename', 'Repository Code of Conduct filename',
-     'Repository Security Threat Model filename', 'Repository Security Audit filename',
-     'Repository Status', 'Repository Last Synced Timestamp', 'Repository SourceRank',
-     'Repository Display Name', 'Repository SCM type', 'Repository Pull requests enabled?',
-     'Repository Logo URL', 'Repository Keywords']
