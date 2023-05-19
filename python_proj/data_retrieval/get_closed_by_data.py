d2a13bc812d150d9e5236b0be0e2ab33ca990243
Willem <w.meijer.5@student.rug.nl>
2023-05-19 11:23:39 +0000

    get clsoed by data

--- python_proj/data_retrieval/get_closed_by_data.py
+++ python_proj/data_retrieval/get_closed_by_data.py
@@ -1,6 +1,7 @@
 """
-Retrieves the "closed_by" fields for pull requests that have been closed.
-This script is only here because I forgot to include the closed_by field in the ``IssueFilter``...
+Retrieves the ``closed_by`` fields for pull requests that have been closed but not merged.
+This script is only here because I forgot to include the ``closed_by`` field in the ``IssueFilter``.
+If you re-run the experiment in its entirety, you don't need this script.
 """
 
 from dataclasses import dataclass
@@ -11,8 +12,9 @@ import requests
 import time
 
 from python_proj.data_retrieval.gl_filters.gl_github_filters import UserFilter
-from python_proj.utils.mt_utils import parallelize_tasks
+from python_proj.utils.arg_utils import safe_get_argv
 import python_proj.utils.exp_utils as exp_utils
+from python_proj.utils.mt_utils import parallelize_tasks
 
 
 @dataclass
@@ -28,7 +30,8 @@ user_filter: UserFilter | None
 
 def get_closed_by(owner: str,
                   repo: str, issue: int,
-                  gh_token: str | None = None) -> dict | None:
+                  gh_token: str | None = None) \
+        -> dict | None:
     global token, user_filter
 
     used_token = None
@@ -60,7 +63,8 @@ def get_closed_by(owner: str,
             time.sleep(300)
             continue
         elif result.status_code // 100 != 2:
-            print(f'Error: {result.status_code}: {result.text}')
+            print(
+                f'Error for ({owner=}, {repo=}, {issue=}): status code {result.status_code} with message "{result.text}"')
             return
         succeeded = True
 
@@ -77,46 +81,72 @@ def get_closed_by(owner: str,
     return filtered
 
 
-def get_closed_by_for_closed_issues():
+def get_closed_by_for_closed_and_unmerged_prs():
+    """
+    Iterates through the chronological data file and retrieves ``closed_by``
+    data for each pull request that has been closed and not merged.
+    """
+
     def __get_owner_and_repo(entry: dict) -> tuple[str, str]:
+        """Helper method for string magic."""
         source_path = entry["__source_path"]
         file_name_with_ext = source_path.split("/")[-1]
-        file_name = file_name_with_ext.split(".")[0]
+        file_name = ".".join(file_name_with_ext.split(".")[:-1])
         owner_repo = file_name.split("--")
         return (owner_repo[0], owner_repo[1])
 
     # Creates tasks
     input_path = exp_utils.CHRONOLOGICAL_DATASET_PATH
     tasks = []
+    total_entries = 0
     with open(input_path, "r") as input_file:
         for line in input_file:
+            total_entries += 1
             entry = json.loads(line)
-            if entry["state"] != "closed":
+            if entry["merged"] != True:
                 continue
             owner, repo = __get_owner_and_repo(entry)
             issue = entry['number']
             new_pr = PullRequestEntry(owner, repo, issue)
             tasks.append(new_pr)
 
+    print(f'{len(tasks)}/{total_entries} ({100 * (len(tasks)/total_entries):.03f}%).')
+
     def __pull_data(task: PullRequestEntry, worker_index: int, gh_tokens: list[list[str]], *args, **kwargs):
+        """Helper method for parallel retrieval and storage of data."""
         my_token = gh_tokens[worker_index]
         closed_by = get_closed_by(
             task.owner, task.repo, task.issue, gh_token=my_token)
         if closed_by is None:
             return
-
+        entry = {"owner": task.owner, "repo": task.repo,
+                 "issue": task.issue, "closed_by": closed_by}
         output_path = f"./data/temp/closed_by_t_{worker_index}.json"
         with open(output_path, "w+") as output_file:
-            output_file.write(f"{json.dumps(closed_by)}\n")
+            output_file.write(f"{json.dumps(entry)}\n")
 
-        raise Exception("asdf")
-
+    # Parallelizes the tasks.
     worker_count = 3
     gh_tokens = exp_utils.get_gh_tokens(worker_count)
     parallelize_tasks(tasks, __pull_data, worker_count, gh_tokens=gh_tokens)
 
 
+def add_closed_by_data_to_prs():
+    pass
+
+
 if __name__ == "__main__":
     dotenv.load_dotenv()
     exp_utils.load_paths_for_all_argv()
-    get_closed_by_for_closed_issues()
+
+    mode = safe_get_argv(key='-m', default="r")
+    match(mode):
+        case "r":
+            get_closed_by_for_closed_and_unmerged_prs()
+        case "m":
+            add_closed_by_data_to_prs()
+        case "b":
+            get_closed_by_for_closed_and_unmerged_prs()
+            add_closed_by_data_to_prs()
+        case _:
+            raise ValueError(f"Invalid mode {mode}.")
