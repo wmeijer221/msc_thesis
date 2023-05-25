08951a722962a133e868677e6cc3a2f54ecf7efc
Willem <w.meijer.5@student.rug.nl>
2023-05-25 07:38:00 +0000

    removed prints

--- python_proj/data_preprocessing/merge_issue_pr_data.py
+++ python_proj/data_preprocessing/merge_issue_pr_data.py
@@ -89,7 +89,6 @@ def do_the_merge(filter_type: str, delete_old: bool = False, write_new: bool = F
                                                      owner=owner,
                                                      repo=repo,
                                                      ext="--with-issue-data")
-            print(pr_output_path)
             with open(pr_output_path, "w+") as output_file:
                 if write_new:
                     output_file.write(json.dumps(new_prs, indent=2))
@@ -106,7 +105,6 @@ def do_the_merge(filter_type: str, delete_old: bool = False, write_new: bool = F
                                                         owner=owner,
                                                         repo=repo,
                                                         ext="--no-prs")
-            print(issue_output_path)
             with open(issue_output_path, "w+") as issue_output_file:
                 data = list(issues_mapping.values())
                 issue_output_file.write(json.dumps(data, indent=2))
