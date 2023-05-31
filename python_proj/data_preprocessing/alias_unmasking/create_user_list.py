a9407ec5bea07a53c398c43caa7330ee546fa0c5
Willem <w.meijer.5@student.rug.nl>
2023-05-31 10:19:48 +0000

    cmt

--- python_proj/data_preprocessing/alias_unmasking/create_user_list.py
+++ python_proj/data_preprocessing/alias_unmasking/create_user_list.py
@@ -16,20 +16,14 @@ def user_list_generator() -> Generator[Tuple[Dict, str], None, None]:
     for entry in exp_utils.iterate_through_chronological_data():
         owner, repo = exp_utils.get_owner_and_repo_from_source_path(
             entry["__source_path"])
-        project_name = f'{owner}/{repo}'
         try:
-            yield entry["user_data"], project_name
+            yield entry["user_data"], owner, repo
             integrator_key = exp_utils.get_integrator_key(entry)
-            # TODO: This can be removed once the dataset is complete.
-            if integrator_key not in entry:
-                pass
-                # print("MISSING INTEGRATOR KEY!!!")
-            else:
-                yield entry[integrator_key], project_name
+            yield entry[integrator_key], owner, repo
             if entry["comments"] == 0 or "comments_data" not in entry:
                 continue
             for comment in entry["comments_data"]:
-                yield comment["user_data"], project_name
+                yield comment["user_data"], owner, repo
         except:
             print(json.dumps(entry))
             raise
@@ -38,7 +32,7 @@ def user_list_generator() -> Generator[Tuple[Dict, str], None, None]:
 def create_user_list():
     unique_users: dict[str, dict] = {}
     user_to_projects: dict[str, set[str]] = {}
-    for (user, project) in user_list_generator():
+    for index, (user, owner, repo) in enumerate(user_list_generator()):
         user_id = user["id"]
         # Store user.
         if user_id not in unique_users:
@@ -46,21 +40,24 @@ def create_user_list():
         # Store project.
         if user_id not in user_to_projects:
             user_to_projects[user_id] = set()
-        user_to_projects[user_id].add(project)
+        user_to_projects[user_id].add((owner, repo))
+        # TODO: THIS IS TEST STUFF.
+        if index > 100:
+            break
 
     def __user_to_entry(user: dict) -> list[str]:
         user_id = safe_get(user, "id", "")
         login = safe_get(user, "login", "")
         name = safe_get(user, "name", "")
         email = safe_get(user, "email", "")
-        projects = ";".join(user_to_projects[user_id])
+        projects = ";".join([f'{owner}/{repo}'
+                             for (owner, repo) in user_to_projects[user_id]])
         return [user_id, login, name, email, projects]
 
-    output_path = exp_utils.BASE_PATH + "/extract/user_ids/{project_name}.csv"
+    output_path = exp_utils.RAW_DATA_PATH
     for user_id, projects in user_to_projects.items():
-        for project in projects:
-            r_output_path = output_path.format(
-                project_name=project.replace("/", '--'))
+        for (owner, repo) in projects:
+            r_output_path = output_path(owner=owner, repo=repo)
 
             # File creation
             if not path.exists(r_output_path):
@@ -68,7 +65,7 @@ def create_user_list():
                     csv_writer = writer(output_file)
                     csv_writer.writerow(["user_id", "login",
                                          "name", "email", "projects"])
-                
+
                 dirname = path.dirname(r_output_path)
                 if not path.exists(dirname):
                     makedirs(dirname)
