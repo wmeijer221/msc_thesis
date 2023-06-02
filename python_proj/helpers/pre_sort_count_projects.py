a0d80f20aa655054a8f17893bed277b92f3c00c9
Willem <w.meijer.5@student.rug.nl>
2023-06-02 07:59:32 +0000

    counter

---
+++ python_proj/helpers/pre_sort_count_projects.py
@@ -0,0 +1,28 @@
+from os import path
+
+import python_proj.utils.exp_utils as exp_utils
+from python_proj.utils.arg_utils import safe_get_argv
+
+
+def count_projects(filter_name: str, file_ext: str):
+    filter_path = exp_utils.FILTER_PATH(filter_name=filter_name)
+    with open(filter_path, "r") as filter_file:
+        filter = {entry.strip() for entry in filter_file}
+
+    total = len(filter)
+    counted = 0
+    for entry in filter:
+        split = entry.split("/")
+        owner, repo = split[0], split[1]
+        file_name = exp_utils.RAW_DATA_PATH(
+            owner=owner, repo=repo, ext=file_ext)
+        if path.exists(file_name):
+            counted += 1
+    
+    print(f'{counted}/{total} projects found.')
+
+if __name__ == "__main__":
+    exp_utils.load_paths_for_all_argv()
+    filter_name = exp_utils.get_file_name()
+    ext = safe_get_argv(key='-x', default="")
+    count_projects(filter_name, ext)
