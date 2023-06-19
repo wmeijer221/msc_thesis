d9386ed71062d8db96b128118a000b3a4adaef38
Willem <w.meijer.5@student.rug.nl>
2023-06-19 07:19:12 +0000

    error

--- python_proj/data_filters/post_sort_filter_for_pr_count.py
+++ python_proj/data_filters/post_sort_filter_for_pr_count.py
@@ -6,7 +6,7 @@ from python_proj.utils.arg_utils import get_argv
 
 def calculate_prs_per_project(input_name: str) -> dict[str, int]:
     ds_iterator = exp_utils.iterate_through_multiple_chronological_datasets(
-        data_sources=[input_name],
+        dataset_names=[input_name],
         dataset_types=[''],
         data_sources=[''])
     pr_count_per_project = {}
