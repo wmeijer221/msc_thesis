49f69a08478a5acd75268fddc744a46f83873bc5
Willem <w.meijer.5@student.rug.nl>
2023-06-19 06:30:40 +0000

    better dd experiment

--- experiment_with_data_distribution.sh
+++ experiment_with_data_distribution.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+echo Removing invalid entries.
+python3 ./python_proj/data_preprocessing/sliding_window_2.py \
+    -m r \
+    -pd sorted_filtered_focal_to_other_complete,sorted_filtered_other_to_focal_sample_1_complete,sorted_other_to_focal_sample_2_complete,sorted_filtered_core_complete \
+    -id sorted_filtered_issues_core,sorted_filtered_issues_core_to_other,sorted_filtered_other_to_core_sample_1,sorted_filtered_other_to_core_sample_2
+
 mkdir ./data/figures/data_distribution_2
 
 echo Starting with all
 python3 ./python_proj/data_preprocessing/sliding_window_2.py \
-    -pd sorted_filtered_focal_to_other_complete,sorted_filtered_other_to_focal_sample_1_complete,sorted_other_to_focal_sample_2_complete,sorted_filtered_core_complete \
-    -id sorted_filtered_issues_core,sorted_filtered_issues_core_to_other,sorted_filtered_other_to_core_sample_1,sorted_filtered_other_to_core_sample_2
+    -pd sorted_filtered_focal_to_other_complete_no_invalid,sorted_filtered_other_to_focal_sample_1_complete_no_invalid,sorted_other_to_focal_sample_2_complete_no_invalid,sorted_filtered_core_complete_no_invalid \
+    -id sorted_filtered_issues_core_no_invalid,sorted_filtered_issues_core_to_other_no_invalid,sorted_filtered_other_to_core_sample_1_no_invalid,sorted_filtered_other_to_core_sample_2_no_invalid
     
     
 python3 ./python_proj/modelling/data_distribution_2.py
@@ -16,16 +22,18 @@ cd ../../..
 
 for value in 30 90 180 365
 do
-    echo Starting with $value
+    echo Starting with $value day window.
+
     python3 ./python_proj/data_preprocessing/sliding_window_2.py \
         -w $value \
-        -pd sorted_filtered_focal_to_other_complete,sorted_filtered_other_to_focal_sample_1_complete,sorted_other_to_focal_sample_2_complete,sorted_filtered_core_complete \
-        -id sorted_filtered_issues_core,sorted_filtered_issues_core_to_other,sorted_filtered_other_to_core_sample_1,sorted_filtered_other_to_core_sample_2
+        -pd sorted_filtered_focal_to_other_complete_no_invalid,sorted_filtered_other_to_focal_sample_1_complete_no_invalid,sorted_other_to_focal_sample_2_complete_no_invalid,sorted_filtered_core_complete_no_invalid \
+        -id sorted_filtered_issues_core_no_invalid,sorted_filtered_issues_core_to_other_no_invalid,sorted_filtered_other_to_core_sample_1_no_invalid,sorted_filtered_other_to_core_sample_2_no_invalid
     python3 ./python_proj/modelling/data_distribution_2.py
+
     cd /data/s4509412/data/data/figures/data_distribution_2/
     mkdir ./d_$value/
     mv ./*.png ./d_$value/
     cd ~/msc_thesis
 done
 
-echo Done
+echo Done!
