b998aeb8be7a52312d87d9b3ceb2abfc01097807
Willem <w.meijer.5@student.rug.nl>
2023-06-19 11:21:20 +0000

    whups

--- bash_scripts/simple-backup.sh
+++ bash_scripts/simple-backup.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-
-echo Starting back-up!
-
 LOGFILE=/data/s4509412/backup-compute-server-logs-$1.txt
 date > $LOGFILE
-rsync -a $LOGFILE --delete --verbose >> $LOGFILE
-
-echo Completed back-up!
+rsync -a /data/s4509412/data /data/s4509412/$1 --delete --verbose >> $LOGFILE
