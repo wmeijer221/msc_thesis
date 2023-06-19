d47baf887148601f0112ed894a19e23c4147031b
Willem <w.meijer.5@student.rug.nl>
2023-06-19 07:26:44 +0000

    more errors

--- python_proj/data_filters/post_sort_filter_for_pr_count.py
+++ python_proj/data_filters/post_sort_filter_for_pr_count.py
@@ -4,9 +4,9 @@ import python_proj.utils.exp_utils as exp_utils
 from python_proj.utils.arg_utils import get_argv, safe_get_argv
 
 
-def calculate_prs_per_project(input_name: str) -> dict[str, int]:
+def calculate_prs_per_project(input_names: list[str]) -> dict[str, int]:
     ds_iterator = exp_utils.iterate_through_multiple_chronological_datasets(
-        dataset_names=input_name)
+        dataset_names=input_names)
     pr_count_per_project = {}
     for entry in ds_iterator:
         project = entry["__source_path"]
@@ -17,13 +17,12 @@ def calculate_prs_per_project(input_name: str) -> dict[str, int]:
 
 
 def filter_for_pr_count(prs_per_project: dict[str, int],
-                        input_name: str,
+                        input_names: list[str],
                         output_path: str,
                         pr_threshold: int):
     with open(output_path, "w+") as output_file:
         ds_iterator = exp_utils.iterate_through_multiple_chronological_datasets(
-            data_sources=[input_name],
-            dataset_types=[''])
+            data_sources=input_names)
         for entry in ds_iterator:
             project = entry["__source_path"]
             if prs_per_project[project] < pr_threshold:
