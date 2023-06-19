f84dbbf6e29fb77d2ac556c693790e8f8b5b6361
Willem <w.meijer.5@student.rug.nl>
2023-06-19 08:34:04 +0000

    plot gen works with exp paths.

--- experiment_with_data_distribution.sh
+++ experiment_with_data_distribution.sh
@@ -12,13 +12,13 @@ echo Starting with all
 python3 ./python_proj/data_preprocessing/sliding_window_2.py \
     -pd sorted_filtered_focal_to_other_complete_no_invalid,sorted_filtered_other_to_focal_sample_1_complete_no_invalid,sorted_filtered_other_to_focal_sample_2_complete_no_invalid,sorted_filtered_core_complete_no_invalid \
     -id sorted_filtered_issues_core_no_invalid,sorted_filtered_issues_core_to_other_no_invalid,sorted_filtered_other_to_core_sample_1_no_invalid,sorted_filtered_other_to_core_sample_2_no_invalid
-    
-    
+ 
 python3 ./python_proj/modelling/data_distribution_2.py
+
 cd /data/s4509412/data/data/figures/data_distribution_2/
 mkdir ./all
 mv ./*.png ./all/
-cd ../../..
+cd ~/msc_thesis
 
 for value in 30 90 180 365
 do
--- python_proj/modelling/data_distribution_2.py
+++ python_proj/modelling/data_distribution_2.py
@@ -1,9 +1,16 @@
-import matplotlib.pyplot as plt
-import numpy as np
+"""Plots distribution data."""
+
 from csv import reader
+from functools import partial
+
+import matplotlib.pyplot as plt
+
+import python_proj.utils.exp_utils as exp_utils
 
 
-def make_plot(feature_name: str, data: list[float]):
+def make_plot(feature_name: str, data: list[float], output_path: str):
+    """Makes a distribution plot for one feature."""
+
     # Create a histogram plot
     plt.cla()
     plt.hist(data, bins=30, density=True, edgecolor='black')
@@ -11,18 +18,32 @@ def make_plot(feature_name: str, data: list[float]):
     # Add labels and title
     plt.xlabel('Value')
     plt.ylabel('Density')
-    plt.title('Distribution Plot')
+    plt.title(f'Distribution Plot {feature_name}')
+    
+    plt.savefig(output_path)
 
-    plt.savefig(f"./data/figures/data_distribution_2/{feature_name}.png")
+
+def make_plots_for_all_features():
+    """Makes plots for all features."""
+
+    exp_utils.load_paths_for_eco()
+    input_path = exp_utils.TRAIN_DATASET_PATH(file_name="test_dataset")
+    figure_base_path: partial[str] = partial(
+        exp_utils.FIGURE_PATH, data_source="distribution_2", file_name="")
+    print(f'Loading data from: "{input_path}".')
+    with open(input_path, "r", encoding='utf-8') as input_file:
+        csv_reader = reader(input_file)
+        header = next(csv_reader)
+        input_data = [[] for _ in range(len(header))]
+        for entry in csv_reader:
+            for index, field in enumerate(entry):
+                input_data[index].append(field)
+
+    for title, input_data in zip(header, input_data):
+        output_path = figure_base_path(figure_name=title)
+        print(f'Creating figure at "{output_path}".')
+        make_plot(title, input_data, output_path)
 
 
-with open("./data/libraries/npm-libraries-1.6.0-2020-01-12/test_dataset.csv", "r") as input_file:
-    csv_reader = reader(input_file)
-    header = next(csv_reader)
-    data = [[] for _ in range(len(header))]
-    for entry in csv_reader:
-        for index, field in enumerate(entry):
-            data[index].append(field)
-
-for title, data in zip(header, data):
-    make_plot(title, data)
+if __name__ == "__main__":
+    make_plots_for_all_features()
