cf177bf2c80380d025ea7ad7b7a1bfbf7373198b
Willem <w.meijer.5@student.rug.nl>
2023-06-19 08:38:29 +0000

    rm

--- experiment_with_data_distribution.sh
+++ experiment_with_data_distribution.sh
@@ -6,8 +6,6 @@ python3 ./python_proj/data_preprocessing/sliding_window_2.py \
     -pd sorted_filtered_focal_to_other_complete,sorted_filtered_other_to_focal_sample_1_complete,sorted_filtered_other_to_focal_sample_2_complete,sorted_filtered_core_complete \
     -id sorted_filtered_issues_core,sorted_filtered_issues_core_to_other,sorted_filtered_other_to_core_sample_1,sorted_filtered_other_to_core_sample_2
 
-mkdir ./data/figures/data_distribution_2
-
 echo Starting with all
 python3 ./python_proj/data_preprocessing/sliding_window_2.py \
     -pd sorted_filtered_focal_to_other_complete_no_invalid,sorted_filtered_other_to_focal_sample_1_complete_no_invalid,sorted_filtered_other_to_focal_sample_2_complete_no_invalid,sorted_filtered_core_complete_no_invalid \
