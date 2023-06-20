83d718164e56eec772e8773983433485964cc36a
Willem <w.meijer.5@student.rug.nl>
2023-06-20 06:59:31 +0000

    made pylint happier

--- python_proj/data_filters/post_sort_filters.py
+++ python_proj/data_filters/post_sort_filters.py
@@ -11,24 +11,25 @@ Commandline arguments:
 """
 
 
-from python_proj.data_retrieval.retrieve_pull_requests import end_date
 import json
 from datetime import datetime
 import re
 from csv import reader
 
+from python_proj.data_retrieval.retrieve_pull_requests import end_date
 from python_proj.utils.arg_utils import get_argv
 from python_proj.utils.exp_utils import build_data_path_from_argv, BASE_PATH
 
 
-def load_data(input_path: str):
-    data = []
-    with open(input_path, "r") as input_data:
+def load_data(data_input_path: str):
+    """loads dataset"""
+    j_data = []
+    with open(data_input_path, "r", encoding='utf-8') as input_data:
         for line in input_data:
             line = line.strip()
             j_entry = json.loads(line)
-            data.append(j_entry)
-    return data
+            j_data.append(j_entry)
+    return j_data
 
 
 def build_filters(selected_filter_keys: str):
@@ -49,6 +50,11 @@ def build_filters(selected_filter_keys: str):
 
 
 def filter_by_close_date(entry):
+    """
+    If it doesn't have a closed_at key it's removed, 
+    and if the date is too new, it is removed as well.
+    """
+
     closed_at_key = 'closed_at'
     if not closed_at_key in entry:
         return False
@@ -58,6 +64,10 @@ def filter_by_close_date(entry):
 
 
 def filter_for_github(entry):
+    """
+    If it's not a github entry, it's removed.
+    """
+
     created_at_key = 'created_at'
     created_date = entry[created_at_key]
     # HACK: GitLab uses a different time format. I've got no better way to test for GH PRs, though.
@@ -69,62 +79,80 @@ def filter_for_github(entry):
 
 
 def filter_for_bots_by_user_type(entry):
+    """
+    If the user type is a bot, it's removed.
+    """
+
     user_data = entry['user_data']
     user_type = user_data['type']
     return user_type.lower() != 'bot'
 
 
-dey_bots_names = None
-dey_bots_emails = None
+DEY_BOTS_NAMES = None
+DEY_BOTS_EMAILS = None
 
 
 def filter_bots_dey_2020(entry):
-    global dey_bots_emails, dey_bots_names
+    """
+    If Dey's (2020) list includes the submitter, it's removed;
+    i.e., when they thin, it's a bot.
+    """
+
+    global DEY_BOTS_EMAILS, DEY_BOTS_NAMES
 
     # HACK: make-shift init function for the filter.
-    if dey_bots_emails is None or dey_bots_emails is None:
+    if DEY_BOTS_EMAILS is None or DEY_BOTS_EMAILS is None:
         filter_path = BASE_PATH + "bot_data/dey_2020_bots.json"
-        with open(filter_path, "r") as input_file:
+        with open(filter_path, "r", encoding='utf-8') as input_file:
             j_data = json.loads(input_file.read())
-            dey_bots_emails = set()
-            dey_bots_names = set()
+            DEY_BOTS_EMAILS = set()
+            DEY_BOTS_NAMES = set()
             for bot_entry in j_data:
-                dey_bots_emails.add(bot_entry['email'])
-                dey_bots_names.add(bot_entry['name'].lower())
+                DEY_BOTS_EMAILS.add(bot_entry['email'])
+                DEY_BOTS_NAMES.add(bot_entry['name'].lower())
 
     # Actual filter
     user_data = entry["user_data"]
-    if ("email" in user_data and user_data["email"] in dey_bots_emails) \
-        or user_data["login"].lower() in dey_bots_names \
-            or ('name' in user_data and user_data["name"].lower() in dey_bots_names):
+    if ("email" in user_data and user_data["email"] in DEY_BOTS_EMAILS) \
+        or user_data["login"].lower() in DEY_BOTS_NAMES \
+            or ('name' in user_data and user_data["name"].lower() in DEY_BOTS_NAMES):
         return False
 
     return True
 
 
-golzadeh_bots_names = None
+GOLZADEH_BOTS_NAMES = None
 
 
 def filter_bots_golzadeh_2021(entry):
-    global golzadeh_bots_names
+    """
+    If Golzadeh's (2020) list includes the submitter, it's removed;
+    i.e., when they thin, it's a bot.
+    """
+
+    global GOLZADEH_BOTS_NAMES
 
     # HACK: make-shift init function.
-    if golzadeh_bots_names is None:
+    if GOLZADEH_BOTS_NAMES is None:
         filter_path = BASE_PATH + "bot_data/golzadeh_2021_bots.csv"
-        with open(filter_path, "r") as filter_file:
+        with open(filter_path, "r", encoding='utf-8') as filter_file:
             filter_reader = reader(filter_file, quotechar='"')
             _ = next(filter_reader)  # Skips header
-            golzadeh_bots_names = {entry[0].lower() for entry in filter_reader}
+            GOLZADEH_BOTS_NAMES = {entry[0].lower() for entry in filter_reader}
 
     # actual filter
     user_data = entry["user_data"]
-    if user_data["login"].lower() in golzadeh_bots_names:
+    if user_data["login"].lower() in GOLZADEH_BOTS_NAMES:
         return False
 
     return True
 
 
 def filter_for_deleted_accounts(entry):
+    """
+    Filters entries submitted by the ghost user.
+    """
+
     user_data = entry['user_data']
     user_login = user_data['login'].lower()
     # Accounts that are deleted are represented by the 'ghost' account. See https://github.com/ghost
@@ -132,12 +160,20 @@ def filter_for_deleted_accounts(entry):
 
 
 def filter_for_blacklist(entry):
+    """
+    Filters if entries are submitted by custom manually found bot.
+    """
+
     user_data = entry['user_data']
     user_login = user_data['login'].lower()
     return not user_login in ["fabric8cd", "mrsflux", "greenkeeperio-bot", "ovh-ux-cds"]
 
 
 def filter_for_bot_in_name(entry):
+    """
+    Filters when the name contains [bot]
+    """
+
     user_data = entry['user_data']
     login = user_data['login']
     for _ in re.finditer('[bot]', login):
@@ -166,9 +202,10 @@ def filter_data(original_data: list, filter_methods: list) -> list:
     return filtered
 
 
-def write_data(data: list, output_path: str):
-    with open(output_path, "w+", encoding='utf-8') as output_file:
-        for entry in data:
+def write_data(output_data: list, output_data_path: str):
+    """writes data to file."""
+    with open(output_data_path, "w+", encoding='utf-8') as output_file:
+        for entry in output_data:
             output_file.write(f'{json.dumps(entry)}\n')
 
 
