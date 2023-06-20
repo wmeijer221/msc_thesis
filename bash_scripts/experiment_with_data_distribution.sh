a589f09199915f56759cab3d8d451898f1ff29b6
Willem <w.meijer.5@student.rug.nl>
2023-06-20 09:21:33 +0000

    filtering w/ skips

--- bash_scripts/experiment_with_data_distribution.sh
+++ bash_scripts/experiment_with_data_distribution.sh
@@ -5,26 +5,26 @@
 # Generate various training datasets with those chronological datasets using different window sizes.
 # Generate figures of the data distribution.
 
-# echo Removing invalid entries.
-# python3 ./python_proj/data_preprocessing/sliding_window_2.py \
-#     -m r \
-#     -pd sorted_filtered_focal_to_other_complete,sorted_filtered_other_to_focal_sample_1_complete,sorted_filtered_other_to_focal_sample_2_complete,sorted_filtered_core_complete \
-#     -id sorted_filtered_issues_core,sorted_filtered_issues_core_to_other,sorted_filtered_other_to_core_sample_1,sorted_filtered_other_to_core_sample_2
+echo Removing invalid entries.
+python3 ./python_proj/data_preprocessing/sliding_window_2.py \
+    -m r \
+    -pd sorted_filtered2_core_complete,sorted_filtered2_focal_to_other_complete,sorted_filtered2_focal_to_other_complete,sorted_filtered2_focal_to_other_complete \
+    -id sorted_filtered2_issues_core_no_invalid,sorted_filtered2_issues_core_no_invalid,sorted_filtered2_issues_core_no_invalid,sorted_filtered2_issues_core_no_invalid
 
 echo Starting with all
 python3 ./python_proj/data_preprocessing/sliding_window_2.py \
-    -pd sorted_filtered_focal_to_other_complete_no_invalid,sorted_filtered_other_to_focal_sample_1_complete_no_invalid,sorted_filtered_other_to_focal_sample_2_complete_no_invalid,sorted_filtered_core_complete_no_invalid \
-    -id sorted_filtered_issues_core_no_invalid,sorted_filtered_issues_core_to_other_no_invalid,sorted_filtered_other_to_core_sample_1_no_invalid,sorted_filtered_other_to_core_sample_2_no_invalid
-python3 ./python_proj/modelling/data_distribution_2.py -s all
+    -pd sorted_filtered2_core_complete_no_invalid,sorted_filtered2_focal_to_other_complete_no_invalid,sorted_filtered2_focal_to_other_complete_no_invalid,sorted_filtered2_focal_to_other_complete_no_invalid \
+    -id sorted_filtered2_issues_core_no_invalid,sorted_filtered2_issues_core_no_invalid,sorted_filtered2_issues_core_no_invalid,sorted_filtered2_issues_core_no_invalid
+python3 ./python_proj/modelling/data_distribution_2.py -s all --skip 4
 
 for value in 30 90 180 365
 do
     echo Starting with $value day window.
     python3 ./python_proj/data_preprocessing/sliding_window_2.py \
         -w $value \
-        -pd sorted_filtered_focal_to_other_complete_no_invalid,sorted_filtered_other_to_focal_sample_1_complete_no_invalid,sorted_filtered_other_to_focal_sample_2_complete_no_invalid,sorted_filtered_core_complete_no_invalid \
-        -id sorted_filtered_issues_core_no_invalid,sorted_filtered_issues_core_to_other_no_invalid,sorted_filtered_other_to_core_sample_1_no_invalid,sorted_filtered_other_to_core_sample_2_no_invalid
-    python3 ./python_proj/modelling/data_distribution_2.py -s d_$value
+        -pd sorted_filtered2_core_complete_no_invalid,sorted_filtered2_focal_to_other_complete_no_invalid,sorted_filtered2_focal_to_other_complete_no_invalid,sorted_filtered2_focal_to_other_complete_no_invalid \
+        -id sorted_filtered2_issues_core_no_invalid,sorted_filtered2_issues_core_no_invalid,sorted_filtered2_issues_core_no_invalid,sorted_filtered2_issues_core_no_invalid
+    python3 ./python_proj/modelling/data_distribution_2.py -s d_$value --skip 4
 done
 
 echo Done!
--- python_proj/modelling/data_distribution_2.py
+++ python_proj/modelling/data_distribution_2.py
@@ -35,18 +35,20 @@ def make_plots_for_all_features():
     exp_utils.load_paths_for_eco()
     input_path = exp_utils.TRAIN_DATASET_PATH(file_name="test_dataset")
     sub_directory_name = safe_get_argv(key="-s", default="")
+    skip_first_x = safe_get_argv(key='--skip', default=0, data_type=int)
     figure_base_path: partial[str] = partial(
         exp_utils.FIGURE_PATH,
-        data_source="distribution_2",
+        data_source="distribution_3",
         file_name=sub_directory_name
     )
     print(f'Loading data from: "{input_path}".')
     with open(input_path, "r", encoding='utf-8') as input_file:
         csv_reader = reader(input_file)
-        header = next(csv_reader)
+        header = next(csv_reader)[skip_first_x:]
         input_data = [[] for _ in range(len(header))]
         for entry in csv_reader:
-            for index, field in enumerate(entry):
+            s_entry = entry[skip_first_x:]
+            for index, field in enumerate(s_entry):
                 input_data[index].append(field)
 
     for title, input_data in zip(header, input_data):
