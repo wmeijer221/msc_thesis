82832a54af459846ccd06887a8be3d1ea69c8216
Willem <w.meijer.5@student.rug.nl>
2023-06-21 07:52:25 +0000

    commas!

--- bash_scripts/experiment_with_data_distribution.sh
+++ bash_scripts/experiment_with_data_distribution.sh
@@ -9,26 +9,14 @@ echo Removing invalid entries.
 
 python3 ./python_proj/data_preprocessing/sliding_window_2.py \
     -m r \
-    -pd sorted_filtered2_core_complete \
-        sorted_filtered2_focal_to_other_complete \
-        sorted_filtered2_other_to_focal_sample_1_complete \
-        sorted_filtered2_other_to_focal_sample_2_complete \
-    -id sorted_filtered2_issues_core \
-        sorted_filtered2_issues_core_to_other \
-        sorted_filtered2_other_to_core_sample_1 \
-        sorted_filtered2_other_to_core_sample_2
+    -pd sorted_filtered2_core_complete,sorted_filtered2_focal_to_other_complete,sorted_filtered2_other_to_focal_sample_1_complete,sorted_filtered2_other_to_focal_sample_2_complete \
+    -id sorted_filtered2_issues_core,sorted_filtered2_issues_core_to_other,sorted_filtered2_other_to_core_sample_1,sorted_filtered2_other_to_core_sample_2
 
 echo Starting with all
 
 python3 ./python_proj/data_preprocessing/sliding_window_2.py \
-    -pd sorted_filtered2_core_complete_no_invalid \
-        sorted_filtered2_focal_to_other_complete_no_invalid \
-        sorted_filtered2_other_to_focal_sample_1_complete_no_invalid \
-        sorted_filtered2_other_to_focal_sample_2_complete_no_invalid \
-    -id sorted_filtered2_issues_core_no_invalid \
-        sorted_filtered2_issues_core_to_other_no_invalid \
-        sorted_filtered2_other_to_core_sample_1_no_invalid \
-        sorted_filtered2_other_to_core_sample_2_no_invalid
+    -pd sorted_filtered2_core_complete_no_invalid,sorted_filtered2_focal_to_other_complete_no_invalid,sorted_filtered2_other_to_focal_sample_1_complete_no_invalid,sorted_filtered2_other_to_focal_sample_2_complete_no_invalid \
+    -id sorted_filtered2_issues_core_no_invalid,sorted_filtered2_issues_core_to_other_no_invalid,sorted_filtered2_other_to_core_sample_1_no_invalid,sorted_filtered2_other_to_core_sample_2_no_invalid
 
 python3 ./python_proj/modelling/data_distribution_2.py -s all --skip 4
 
@@ -38,14 +26,8 @@ do
 
     python3 ./python_proj/data_preprocessing/sliding_window_2.py \
         -w $value \
-        -pd sorted_filtered2_core_complete_no_invalid \
-            sorted_filtered2_focal_to_other_complete_no_invalid \
-            sorted_filtered2_other_to_focal_sample_1_complete_no_invalid \
-            sorted_filtered2_other_to_focal_sample_2_complete_no_invalid \
-        -id sorted_filtered2_issues_core_no_invalid \
-            sorted_filtered2_issues_core_to_other_no_invalid \
-            sorted_filtered2_other_to_core_sample_1_no_invalid \
-            sorted_filtered2_other_to_core_sample_2_no_invalid
+        -pd sorted_filtered2_core_complete_no_invalid,sorted_filtered2_focal_to_other_complete_no_invalid,sorted_filtered2_other_to_focal_sample_1_complete_no_invalid,sorted_filtered2_other_to_focal_sample_2_complete_no_invalid \
+        -id sorted_filtered2_issues_core_no_invalid,sorted_filtered2_issues_core_to_other_no_invalid,sorted_filtered2_other_to_core_sample_1_no_invalid,sorted_filtered2_other_to_core_sample_2_no_invalid
 
     python3 ./python_proj/modelling/data_distribution_2.py -s d_$value --skip 4
 done
