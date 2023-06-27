aecd6557e20d206a5cbb130e52ca3ba72a7891e0
Willem <w.meijer.5@student.rug.nl>
2023-06-27 11:45:01 +0000

    dependency

--- python_proj/helpers/measure_dependency_relevance.py
+++ python_proj/helpers/measure_dependency_relevance.py
@@ -20,29 +20,47 @@ from python_proj.utils.util import OpenMany
 from python_proj.data_preprocessing.sliding_window_features.dependent_ecosystem_experience import load_dependency_map
 
 
-def calculate_periphery_to_core_dependencies():
+def calculate_periphery_to_core_dependencies(
+    included_projects_filter_name: str,
+    core_filter_name: str,
+    periphery_filter_names: list[str]
+):
+    """
+    Calculates the number of projects that have a dependency.
+    Included projects  represent the projects that are actually part of the data set (i.e., post-filter).
+    Core and periphery projects speak for themselves.
+    """
+
     exp_utils.load_paths_for_eco()
+
     dependency_map, inv_dependency_map, project_name_to_id_map = load_dependency_map()
 
-    periphery_filters = [entry for entry in get_argv(
-        key="-p").split(",") if entry != ""]
-    core_filter = get_argv(key='-f')
+    included_projects_filter_path = exp_utils.RAW_DATA_PATH(
+        data_type="user-ids",
+        owner='proj',
+        repo="list",
+        ext=included_projects_filter_name
+    )
+    with open(included_projects_filter_path, "r", encoding='utf-8') as included_projects_file:
+        included_projects = {project_name_to_id_map[entry.strip().lower()]
+                             for entry in included_projects_file}
 
-    periphery_filter_paths = [exp_utils.FILTER_PATH(filter_type=filter_name)
-                              for filter_name in periphery_filters]
-    core_filter_path = exp_utils.FILTER_PATH(filter_type=core_filter)
-
+    core_filter_path = exp_utils.FILTER_PATH(filter_type=core_filter_name)
     with open(core_filter_path, "r", encoding='utf-8') as core_file:
         core_projects = {project_name_to_id_map[entry.strip().lower()]
                          for entry in core_file}
+
+    periphery_filter_paths = [exp_utils.FILTER_PATH(filter_type=filter_name)
+                              for filter_name in periphery_filter_names]
 
     dependency_counter = {proj: {'dep': 0, 'inv_dep': 0}
                           for proj in periphery_filter_paths}
     no_mapping_key = 'No Mapping'
     dependency_counter[no_mapping_key] = 0
+    not_included_key = 'Not Included'
+    dependency_counter[not_included_key] = 0
 
     with OpenMany(periphery_filter_paths, "r", encoding='utf-8') as periphery_files:
-        
         for (periphery_file, filter_path) in zip(periphery_files, periphery_filter_paths):
             for entry in periphery_file.readlines():
                 entry = entry.strip().lower()
@@ -52,6 +70,11 @@ def calculate_periphery_to_core_dependencies():
                     continue
 
                 entry_id = project_name_to_id_map[entry]
+
+                if not entry_id in included_projects:
+                    dependency_counter[not_included_key] += 1
+                    continue
+
                 dependencies = dependency_map[entry_id]
                 for dependency in dependencies:
                     if dependency in core_projects:
@@ -68,4 +91,12 @@ def calculate_periphery_to_core_dependencies():
 
 
 if __name__ == "__main__":
-    calculate_periphery_to_core_dependencies()
+    __periphery_filter_names = [entry for entry in get_argv(
+        key="-p").split(",") if entry != ""]
+    __core_filter_name = get_argv(key='-f')
+    __included_projects_name = get_argv(key='-i')
+
+    calculate_periphery_to_core_dependencies(
+        __included_projects_name,
+        __core_filter_name,
+        __periphery_filter_names)
