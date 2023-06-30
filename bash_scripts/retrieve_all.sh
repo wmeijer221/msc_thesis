cfe0a7eadbbed2fb3655fac77b44eb7e8204233f
Willem <w.meijer.5@student.rug.nl>
2023-06-30 20:02:07 +0000

    more data retrieval

---
+++ bash_scripts/retrieve_all.sh
@@ -0,0 +1,5 @@
+
+python3 ./python_proj/data_retrieval/retrieve_pull_requests.py -f periphery_2_dependencies -t 3
+python3 ./python_proj/data_retrieval/retrieve_issues.py -f _periphery_2_dependencies -t 3
+python3 ./python_proj/data_retrieval/retrieve_pull_requests.py -f periphery_2_inv_dependencies -t 3
+python3 ./python_proj/data_retrieval/retrieve_issues.py -f _periphery_2_inv_dependencies -t 3
