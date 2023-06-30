96614698c027a99136d10a83e91fc48ee7c65b41
Willem <w.meijer.5@student.rug.nl>
2023-06-30 19:24:11 +0000

    better stuff

--- python_proj/data_retrieval/get_dependency_periphery_2.py
+++ python_proj/data_retrieval/get_dependency_periphery_2.py
@@ -9,16 +9,18 @@ from python_proj.data_preprocessing.sliding_window_features.dependent_ecosystem_
 
 def get_dependency_periphery(
         source_project_list_name: str,
-        filter_project_list_names: list[str]):
+        filter_project_list_names: list[str],
+        dependency_projects_out_name: str,
+        inv_dependency_projects_out_name: str):
 
     # loads source
     source_project_list_path = exp_utils.RAW_DATA_PATH(
-        data_type='user-ids', owner='proj', repo='list', ext=source_project_list_name)
+        data_type='user-ids', owner='proj', repo='list', ext=source_project_list_name):
     with open(source_project_list_path, "r", encoding='utf-8') as source_project_file:
         projects = {entry.strip() for entry in source_project_file}
 
     # loads projects
-    filter_project_paths = [exp_utils.FILTER_PATH(filter_name=entry)
+    filter_project_paths = [exp_utils.FILTER_PATH(filter_type=entry)
                             for entry in filter_project_list_names]
     with OpenMany(filter_project_paths, "r", encoding='utf-8') as filter_project_files:
         filtered_projects = set()
@@ -28,6 +30,7 @@ def get_dependency_periphery(
                 filtered_projects.add(entry)
 
     dependencies, inv_dependencies, project_name_to_id = safe_load_dependency_map()
+    project_id_to_name = {id: name for name, id in project_name_to_id.items()}
 
     # Creates list of dependency projects.
     potential_dependency_projects = set()
@@ -42,7 +45,8 @@ def get_dependency_periphery(
             project_dependencies = dependencies[project_id]
             for source_project in project_dependencies:
                 # Ignores filtered projects (i.e., previously collected ones)
-                if source_project in filtered_projects:
+                if source_project in filtered_projects \
+                    or source_project not in project_id_to_name:
                     continue
                 potential_dependency_projects.add(source_project)
 
@@ -52,12 +56,17 @@ def get_dependency_periphery(
             inv_project_dependencies = inv_dependencies[project_id]
             for sink_project in inv_project_dependencies:
                 # Ignores filtered projects (i.e., previously collected ones)
-                if sink_project in filtered_projects:
+                # or when there's no id to the name.
+                if sink_project in filtered_projects \
+                    or sink_project not in project_id_to_name:
                     continue
                 potential_inv_dependency_projects.add(sink_project)
 
     print(f'{len(potential_dependency_projects)=}')
     print(f'{len(potential_inv_dependency_projects)=}')
+
+    for project_id in potential_dependency_projects:
+        # project_name = 
 
 
 if __name__ == "__main__":
@@ -67,5 +76,10 @@ if __name__ == "__main__":
     __filter_project_list_names = [entry.strip()
                                    for entry in get_argv(key='-f').split(",")]
 
+    __dependency_projects_out_name = get_argv(key='-do')
+    __inv_dependency_projects_out_name = get_argv(key="-io")
+
     get_dependency_periphery(__source_project_list_name,
-                             __filter_project_list_names)
+                             __filter_project_list_names,
+                             __dependency_projects_out_name,
+                             __inv_dependency_projects_out_name)
