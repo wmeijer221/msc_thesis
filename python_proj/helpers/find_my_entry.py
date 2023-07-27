34bc9ceda62612518bda18fc9be2bf1124a0e9fc
Willem <w.meijer.5@student.rug.nl>
2023-07-27 15:21:54 +0000

    fix

--- python_proj/helpers/find_my_entry.py
+++ python_proj/helpers/find_my_entry.py
@@ -4,9 +4,11 @@ from python_proj.utils.arg_utils import get_argv
 import python_proj.utils.exp_utils as exp_utils
 
 
-def find_in(file: str, id: int):
-    file_path = exp_utils.CHRONOLOGICAL_DATASET_PATH(file_name=file)
+def load(file: str):
+    return {entry: ['id'] for entry in exp_utils.iterate_through_chronological_data(data_type="pull-requests", file_name=file)}
 
+
+def find_in(file: str, id: int):
     for entry in exp_utils.iterate_through_chronological_data(data_type="pull-requests", file_name=file):
         if entry['id'] == id:
             print(json.dumps(entry, indent=4))
@@ -16,7 +18,14 @@ def find_in(file: str, id: int):
 if __name__ == "__main__":
     exp_utils.load_paths_for_eco()
     exp_utils.load_paths_for_data_path()
-    id = get_argv('-id')
     file = get_argv('-f')
 
-    find_in(file, id)
+    data = load(file)
+
+    while True:
+        id = input('fill in PR id:')
+        id = int(id)
+        if id in data:
+            print(json.dumps(data[id], indent=4))
+        else:
+            print('doesnt exist')
