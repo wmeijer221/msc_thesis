229907617c123ebe2babc38d2fd5e3a1d8047442
Willem <w.meijer.5@student.rug.nl>
2023-08-04 11:47:48 +0000

    data compressor

--- python_proj/helpers/data_compresser.py
+++ python_proj/helpers/data_compresser.py
@@ -9,22 +9,27 @@ import json
 
 def shrink_json_in_directory():
     directory = get_argv('--dir')
+    print(f'Shrinking JSON files in "{directory}"')
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
+        print(file_path)
         if not os.path.isfile(file_path):
             continue
+        try:
+            with open(file_path, "r", encoding='utf-8') as file:
+                j_data = json.loads(file.read())
+        except json.JSONDecodeError:
+            print(f'File "{file_path}" is no JSON.')
+            continue
 
-        with open(file_path, "rw", encoding='utf-8') as file:
-            j_data = json.loads(file.read())
-            file.write(json.dumps(j_data, separators=''))
-
-
-
-
+        os.remove(file_path)
+        with open(file_path, "w+", encoding='utf-8') as file:
+            file.write(json.dumps(j_data, separators=(',', ':')))
 
 
 if __name__ == "__main__":
-    
-    jdata = {'key_a': "asdff,", "key_b": 134}
-    print(json.dumps(jdata, separators=''))
-
+    match get_argv('-m'):
+        case "shrink_json":
+            shrink_json_in_directory()
+        case _:
+            raise ValueError("invalid mode")
