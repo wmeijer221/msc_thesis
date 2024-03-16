0ce52ba8e129ce1af3af3211d12247a6d48d6b52
Willem <w.meijer.5@student.rug.nl>
2024-03-16 12:36:41 +0000

    test prominence cascading dependencies

--- python_proj/data_preprocessing/sliding_window_features/dependency_ecosystem_experience/dependency_loading.py
+++ python_proj/data_preprocessing/sliding_window_features/dependency_ecosystem_experience/dependency_loading.py
@@ -18,10 +18,9 @@ DEPENDENCY_MAP: SafeDict[int, set[int]] = None
 PROJECT_NAME_TO_ID: dict[str, int] = None
 
 
-def __attempt_quick_load_dependency_map(
-) -> Tuple[bool, str,
-           SafeDict[int, set[int]] | None,
-           dict[str, int] | None]:
+def __attempt_quick_load_dependency_map() -> (
+    Tuple[bool, str, SafeDict[int, list[int]] | None, dict[str, int] | None]
+):
     """
     Attempts to load the file in which the summarized dependency data is stored.
     Using this file is much, much faster than loading the libraries.io datasets
@@ -34,8 +33,7 @@ def __attempt_quick_load_dependency_map(
     # if it exists. This is much faster than iterating
     # through the whole 15GB datafile.
     ql_dependency_file_name = "ql_dependencies"
-    ql_dependency_path = exp_utils.TRAIN_DATASET_PATH(
-        file_name=ql_dependency_file_name)
+    ql_dependency_path = exp_utils.TRAIN_DATASET_PATH(file_name=ql_dependency_file_name)
 
     print(f'Loading projects and dependencies from: "{ql_dependency_path}".')
 
@@ -44,10 +42,11 @@ def __attempt_quick_load_dependency_map(
         return False, ql_dependency_path, None, None
 
     try:
-        with open(ql_dependency_path, "r", encoding='utf-8') as input_file:
+        with open(ql_dependency_path, "r", encoding="utf-8") as input_file:
             j_data = json.loads(input_file.read())
-            dependency_map = SafeDict(initial_mapping=j_data["dependency_map"],
-                                      default_value=set)
+            dependency_map = SafeDict(
+                initial_mapping=j_data["dependency_map"], default_value=set
+            )
             project_name_to_id = j_data["project_name_to_id"]
 
         print("Finished quick load!")
@@ -58,13 +57,13 @@ def __attempt_quick_load_dependency_map(
         return False, ql_dependency_path, None, None
 
 
-def __slow_load_dependency_map(output_path: str) \
-    -> Tuple[SafeDict[int, set[int]],
-             dict[str, int]]:
+def __slow_load_dependency_map(
+    output_path: str,
+) -> Tuple[SafeDict[int, set[int]], dict[str, int]]:
     """
     Loads all depedency data from the libraries.io dataset.
     This includes the project dependencies and the repository dependencies.
-    Both are included to create a full picture. It also writes the results 
+    Both are included to create a full picture. It also writes the results
     to a quick-load file, so the next run is much faster.
     """
 
@@ -73,16 +72,19 @@ def __slow_load_dependency_map(output_path: str) \
     dependency_map = SafeDict(default_value=set)
 
     # Loads proj id -> proj name mapping.
-    project_with_repo_details_file_name = "projects_with_repository_fields-1.6.0-2020-01-12"
+    project_with_repo_details_file_name = (
+        "projects_with_repository_fields-1.6.0-2020-01-12"
+    )
     project_file_name = exp_utils.TRAIN_DATASET_PATH(
-        file_name=project_with_repo_details_file_name)
+        file_name=project_with_repo_details_file_name
+    )
 
     # Creates a mapping of project names to IDs as dependencies are
     # stored in IDs, and the PR data works only uses names.
     print(f'Loading projects from: "{project_file_name}".')
     project_name_to_id = {}
     repo_name_index = exp_utils.repo_name_index
-    with open(project_file_name, "r", encoding='utf-8') as project_file:
+    with open(project_file_name, "r", encoding="utf-8") as project_file:
         csv_reader = reader(project_file)
         for project in csv_reader:
             repo_id = project[0]
@@ -93,10 +95,9 @@ def __slow_load_dependency_map(output_path: str) \
 
     # Loads project dependencies as logged by NPM.
     dependency_file_name = "dependencies-1.6.0-2020-01-12"
-    dependency_path = exp_utils.TRAIN_DATASET_PATH(
-        file_name=dependency_file_name)
-    print(f'Loading dependencies from: {dependency_path}.')
-    with open(dependency_path, "r", encoding='utf-8') as input_file:
+    dependency_path = exp_utils.TRAIN_DATASET_PATH(file_name=dependency_file_name)
+    print(f"Loading dependencies from: {dependency_path}.")
+    with open(dependency_path, "r", encoding="utf-8") as input_file:
         csv_reader = reader(input_file)
         header = next(csv_reader)
         focal_project_id_idx = header.index("Project ID")
@@ -113,7 +114,8 @@ def __slow_load_dependency_map(output_path: str) \
     # Loads repository dependencies as logged in the respective social coding platform (e.g. GitHub).
     repo_dependency_file_name = "repository_dependencies-1.6.0-2020-01-12"
     repo_dependency_path = exp_utils.TRAIN_DATASET_PATH(
-        file_name=repo_dependency_file_name)
+        file_name=repo_dependency_file_name
+    )
     print(f'Loading repo dependencies from "{repo_dependency_path}".')
     with open(repo_dependency_path, "r", encoding="utf-8") as repo_dependency_file:
         csv_reader = reader(repo_dependency_file)
@@ -133,9 +135,9 @@ def __slow_load_dependency_map(output_path: str) \
             dependee_id = project_name_to_id[dependee_repo_name]
             dependency_map[dependee_id].add(dependency_id)
             dependency_count += 1
-        print(f'{missing_projects=}')
+        print(f"{missing_projects=}")
 
-    print(f'Loaded {dependency_count} dependencies.')
+    print(f"Loaded {dependency_count} dependencies.")
     print(f"Creating quick load file at '{output_path}'.")
 
     # Transforms sets to lists as sets aren't JSON serializable.
@@ -145,9 +147,9 @@ def __slow_load_dependency_map(output_path: str) \
     # Writes the output to a quick-load file.
     ql_dependency_map = {
         "dependency_map": dependency_map,
-        "project_name_to_id": project_name_to_id
+        "project_name_to_id": project_name_to_id,
     }
-    with open(output_path, "w+", encoding='utf-8') as output_file:
+    with open(output_path, "w+", encoding="utf-8") as output_file:
         output_file.write(json.dumps(ql_dependency_map))
 
     print("Finished slow load!")
@@ -155,9 +157,7 @@ def __slow_load_dependency_map(output_path: str) \
     return dependency_map, project_name_to_id
 
 
-def load_dependency_map() \
-    -> Tuple[SafeDict[int, set[int]],
-             dict[str, int]]:
+def load_dependency_map() -> Tuple[SafeDict[int, set[int]], dict[str, int]]:
     """
     Loads the dependency map by first attempting the quick way,
     and if that fails by loading the slow way.
@@ -167,14 +167,18 @@ def load_dependency_map() \
 
     exp_utils.load_paths_for_eco()
     start = datetime.datetime.now()
-    ql_success, ql_dependency_path, dependency_map, project_name_to_id = __attempt_quick_load_dependency_map()
+    ql_success, ql_dependency_path, dependency_map, project_name_to_id = (
+        __attempt_quick_load_dependency_map()
+    )
     if not ql_success:
         dependency_map, project_name_to_id = __slow_load_dependency_map(
-            ql_dependency_path)
+            ql_dependency_path
+        )
     timedelta = datetime.datetime.now() - start
     print(
-        f'Loaded {len(project_name_to_id)} projects and {len(dependency_map)} projects with dependencies.')
-    print(f'Loaded dependency data in {timedelta}.')
+        f"Loaded {len(project_name_to_id)} projects and {len(dependency_map)} projects with dependencies."
+    )
+    print(f"Loaded dependency data in {timedelta}.")
 
     DEPENDENCY_MAP = dependency_map
     PROJECT_NAME_TO_ID = project_name_to_id
@@ -182,9 +186,7 @@ def load_dependency_map() \
     return dependency_map, project_name_to_id
 
 
-def safe_load_dependency_map() \
-    -> Tuple[SafeDict[int, set[int]],
-             dict[str, int]]:
+def safe_load_dependency_map() -> Tuple[SafeDict[int, set[int]], dict[str, int]]:
     """
     Returns the dependency map that in mamory and
     loads it from the filesystem if it hasn't been loaded yet.
@@ -194,3 +196,55 @@ def safe_load_dependency_map() \
         return load_dependency_map()
     else:
         return DEPENDENCY_MAP, PROJECT_NAME_TO_ID
+
+
+def calculate_transitive_dependency_map(
+    dependencies: SafeDict[int, list[int]], max_iter: int = 10_000
+) -> SafeDict[int, set[int]]:
+    """
+    Calculates the transitive closure of the input dependencies;
+    i.e., cascading dependencies.
+    """
+
+    # Changes inner type to set instead of list.
+    for key in dependencies.keys():
+        dependencies[key] = set(dependencies[key])
+
+    # Calculates transitive dependencies.
+    not_converged = True
+    iter = 0
+    while not_converged:
+        iter += 1
+        not_converged = __calculate_transitive_dependency_map(dependencies)
+        if iter >= max_iter:
+            print(f"Could not converge in {max_iter} iterations.")
+            break
+    print(f"Calculated transitive dependency map in {iter} iterations.")
+    return dependencies
+
+
+def __calculate_transitive_dependency_map(
+    dependencies: SafeDict[int, set[int]]
+) -> bool:
+    """
+    Helper function applyling one round of transitive dependency merging.
+    Returns true if a dependency was added.
+    """
+
+    was_updated = False
+
+    project_ids = set(dependencies.keys())
+
+    for project_id in project_ids:
+        deps = dependencies[project_id]
+        old_dep_count = len(deps)
+
+        for dep_id in deps:
+            cascade_deps = dependencies[dep_id]
+            dependencies[project_id] = dependencies[project_id].union(cascade_deps)
+
+        new_dep_count = len(dependencies[project_id])
+        if new_dep_count != old_dep_count:
+            was_updated = True
+
+    return was_updated
